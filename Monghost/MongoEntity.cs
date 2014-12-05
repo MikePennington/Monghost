d1422044235bd77a80527327a9ebccdfc0d44398
Mike Pennington <MikePennington@gamestop.com>
2014-12-05 13:26:23 -0600

    Initial code commit

---
+++ Monghost/MongoEntity.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using MongoDB.Bson;
+
+namespace MonGhost
+{
+    public abstract class MongoEntity
+    {
+        private static readonly Dictionary<Type, string> CollectionNameMap = new Dictionary<Type, string>();
+
+        public abstract ObjectId Id { get; set; }
+
+        public string GetCollectionName()
+        {
+            if (CollectionNameMap.ContainsKey(GetType()))
+                return CollectionNameMap[GetType()];
+
+            var name = LowerAndPluralizeType();
+            AddToMap(name);
+            return name;
+        }
+
+        private void AddToMap(string name)
+        {
+            CollectionNameMap.Add(GetType(), name);
+        }
+
+        private string LowerAndPluralizeType()
+        {
+            var name = GetType().Name.ToLower();
+
+            // For some words we need to look at more than just the last letter
+            if (name.EndsWith("tch"))
+            {
+                name = name + "es";
+                AddToMap(name);
+                return name;
+            }
+
+            // Look at the last letter to determine how to pluralize
+            var lastLetter = name.Substring(name.Length - 1);
+            switch (lastLetter)
+            {
+                case "y":
+                    int index = name.LastIndexOf("y");
+                    name = name.Remove(index, 1).Insert(index, "ies");
+                    return name;
+                case "s":
+                case "o":
+                case "x":
+                    name = name + "es";
+                    return name;
+                default:
+                    name = name + "s";
+                    return name;
+            }
+        }
+    }
+}
---
+++ Monghost/MongoHelper.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+
+namespace MonGhost
+{
+    public interface IMongoHelper
+    {
+        MongoDatabase Database { get; }
+        MongoCollection<T> GetCollection<T>() where T : MongoEntity;
+        T FindOne<T>(ObjectId id) where T : MongoEntity;
+        T FindOne<T>(IMongoQuery query) where T : MongoEntity;
+        List<T> Find<T>(IMongoQuery query) where T : MongoEntity;
+        void Save<T>(T entity) where T : MongoEntity;
+        void Insert<T>(T entity) where T : MongoEntity;
+        void Remove<T>(T entity) where T : MongoEntity;
+    }
+
+    public class MongoHelper : IMongoHelper
+    {
+        private static readonly Dictionary<Type, string> CollectionNameMap = new Dictionary<Type, string>();
+
+        private static MongoDatabase _database;
+
+        public MongoHelper(string connectionString, string databaseName)
+        {
+            if (_database == null)
+            {
+                var client = new MongoClient(connectionString);
+                var server = client.GetServer();
+                _database = server.GetDatabase(databaseName);
+            }
+        }
+
+        // Database is a singlton
+        public MongoDatabase Database { get { return _database; } }
+
+        public MongoCollection<T> GetCollection<T>() where T : MongoEntity
+        {
+            // Check dictionary first
+            string collectionName;
+            if (CollectionNameMap.ContainsKey(typeof(T)))
+            {
+                collectionName = CollectionNameMap[typeof(T)];
+            }
+            else
+            {
+                collectionName = ((T)Activator.CreateInstance(typeof(T))).GetCollectionName();
+                CollectionNameMap.Add(typeof(T), collectionName);
+            }
+
+            var collection = Database.GetCollection<T>(collectionName);
+            return collection;
+        }
+
+        public T FindOne<T>(ObjectId id) where T : MongoEntity
+        {
+            var collection = GetCollection<T>();
+            var query = Query<T>.EQ(x => x.Id, id);
+            return collection.FindOne(query);
+        }
+
+        public T FindOne<T>(IMongoQuery query) where T : MongoEntity
+        {
+            var collection = GetCollection<T>();
+            return collection.FindOne(query);
+        }
+
+        public List<T> Find<T>(IMongoQuery query) where T : MongoEntity
+        {
+            var collection = GetCollection<T>();
+            return collection.Find(query).ToList();
+        }
+
+        public void Save<T>(T entity) where T : MongoEntity
+        {
+            var collection = GetCollection<T>();
+            collection.Save(entity);
+        }
+
+        public void Insert<T>(T entity) where T : MongoEntity
+        {
+            var collection = GetCollection<T>();
+            collection.Insert(entity);
+        }
+
+        public void Remove<T>(T entity) where T : MongoEntity
+        {
+            var collection = GetCollection<T>();
+            var query = Query<T>.EQ(x => x.Id, entity.Id);
+            collection.Remove(query);
+        }
+    }
+}
